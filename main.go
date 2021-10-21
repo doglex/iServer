262ae7a6e569affc833f5a0f33ac7f354203b3a1
Dogless <awsxsa@hotmail.com>
2021-10-21 23:35:28 +0800

    wg function

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os/exec"
+)
 
 func main() {
 	r := gin.Default()
@@ -14,5 +18,40 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/reset_wg", func(c *gin.Context) {
+		command := "./reset_wg.sh"
+		fmt.Println(command)
+		cmd := exec.Command("/bin/bash", "-c", "")
+		_, err := cmd.Output()
+		if err != nil {
+			fmt.Printf("Execute Shell:%s failed with error:%s", command, err.Error())
+			c.JSON(200, gin.H{
+				"message": "error",
+			})
+			return
+		}
+		fmt.Printf("Execute Shell:%s finished")
+		c.JSON(200, gin.H{
+			"message": "reset",
+		})
+	})
+	r.GET("/show_wg", func(c *gin.Context) {
+		command := " /etc/wireguard/wg0_client"
+		fmt.Println(command)
+		cmd := exec.Command("cat", "-c", "")
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Printf("Execute Shell:%s failed with error:%s", command, err.Error())
+			c.JSON(200, gin.H{
+				"message": "error",
+			})
+			return
+		}
+		fmt.Printf("Execute Shell:%s finished")
+		c.JSON(200, gin.H{
+			"message": output,
+		})
+	})
+
 	r.Run("0.0.0.0:80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
---
+++ reset_wg.sh
@@ -0,0 +1,3 @@
+/opt/wireguard.sh -n
+/opt/wireguard.sh -s
+
